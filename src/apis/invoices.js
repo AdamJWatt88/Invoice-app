a14c997076a1965dd58e9aaa71ce20e488b55e40
Adam <adamjwatt88@gmail.com>
2022-01-09 20:09:50 -0800

    changed status style

--- src/apis/invoices.js
+++ src/apis/invoices.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://invoice-app-88.herokuapp.com",
-  baseURL: "https://localhost:3000",
+  baseURL: "https://invoice-app-88.herokuapp.com",
 });
