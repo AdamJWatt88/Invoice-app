395c18795442c02619dda43ce2d0edf1fdcaea4f
Adam <adamjwatt88@gmail.com>
2022-01-13 23:11:13 -0800

    changed server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/db", middlewares, router);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./build")));
   app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./build", "index.html"));
   });
 }
 
