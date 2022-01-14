f0b4843d4eb975f5a9fc15337e72e6162cf489b7
Adam <adamjwatt88@gmail.com>
2022-01-13 22:41:24 -0800

    changed server.js

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 require("dotenv").config();
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults({ static: "build" });
-// const PORT = process.env.PORT || 3000;
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
-
 const jsonServer = require("json-server");
-const app = jsonServer.create();
-const path = require("path");
-const express = require("express");
-const middlewares = jsonServer.defaults({ static: "build" });
+const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults({ static: "build" });
 const PORT = process.env.PORT || 3000;
 
-app.use("/invoices", middlewares, router);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./build")));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./build", "index.html"));
-  });
-}
+server.use(middlewares);
+server.use(router);
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+server.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+
+// const jsonServer = require("json-server");
+// const app = jsonServer.create();
+// const path = require("path");
+// const express = require("express");
+// const middlewares = jsonServer.defaults({ static: "build" });
+// const router = jsonServer.router("db.json");
+// const PORT = process.env.PORT || 3000;
+
+// app.use("/invoices", middlewares, router);
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "./build")));
+//   app.get("/*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "./build", "index.html"));
+//   });
+// }
+
+// app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
