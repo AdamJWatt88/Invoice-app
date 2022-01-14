d8e85291099ab48a9759acde1d2c2e4d589f640f
Adam <adamjwatt88@gmail.com>
2022-01-13 17:39:49 -0800

    added express

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const middlewares = jsonServer.defaults();
 const router = jsonServer.router("db.json");
 const port = process.env.PORT || 3000;
 
-app.use("/db", middlewares, router);
+app.use("/invoices", middlewares, router);
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function (req, res) {
