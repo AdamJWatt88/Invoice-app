a9621a76fab4139c16cce0bd3c9f64c8f0c694e7
Adam <adamjwatt88@gmail.com>
2022-01-13 23:06:00 -0800

    changed server.js

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ const middlewares = jsonServer.defaults();
 const router = jsonServer.router("db.json");
 const PORT = process.env.PORT || 3000;
 
-// app.use("/invoices", middlewares, router);
-app.use(middlewares);
-app.use(router);
+app.use("/db", middlewares, router);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./build")));
   app.get("/*", function (req, res) {
