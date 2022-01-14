53693489bc4774eae9cf73d73ebf906f67c474e2
Adam <adamjwatt88@gmail.com>
2022-01-13 23:37:41 -0800

    changed server.js

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ const jsonServer = require("json-server");
 const app = jsonServer.create();
 const path = require("path");
 const express = require("express");
-const middlewares = jsonServer.defaults({ static: "build" });
+const middlewares = jsonServer.defaults();
 const router = jsonServer.router("db.json");
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json({ extended: false }));
+
+app.use(express.static(__dirname + "/build"));
 
 app.use("/invoices", middlewares, router);
 if (process.env.NODE_ENV === "production") {
