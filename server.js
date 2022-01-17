a41c8b05cfb49239dfe9abaea6728fb05fbc43b9
Adam <adamjwatt88@gmail.com>
2022-01-16 18:36:20 -0800

    changed server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(router);
-
+app.use(express.static("build"));
 // app.use("/", express.static("/public/index.html"), router);
 // app.use("/invoices", require("./routes/invoices"));
 
