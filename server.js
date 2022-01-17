07d631c025a90f2d8db1acd4128ceb02e2386402
Adam <adamjwatt88@gmail.com>
2022-01-16 17:43:17 -0800

    changed server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.json({ extended: false }));
 //? this works but is commented out for now
 // app.use(express.static("build"));
 
-app.use(express.static("build", router));
+app.use("/", express.static("build", router));
 
 //! seems like this is the only problem left. it's this code that doesnt work to redirect to the index.html when the wroung url is input.
 if (process.env.NODE_ENV === "production") {
