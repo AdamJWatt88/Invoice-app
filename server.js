862d7f81de7c1ebbd0d6d50fb0564a13fa0425af
Adam <adamjwatt88@gmail.com>
2022-01-16 19:10:59 -0800

    changed server.js

--- server.js
+++ server.js
@@ -55,9 +55,9 @@ app.use(router);
 
 //! seems like this is the only problem left. it's this code that doesnt work to redirect to the index.html when the wroung url is input.
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname + "/build")));
+  app.use(express.static(path.join(__dirname, "build")));
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/build/index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
 
