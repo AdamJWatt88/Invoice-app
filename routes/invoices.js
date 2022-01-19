3a4f0cccdd5eca952da6edc12ea4a5c0168c38fa
Adam <adamjwatt88@gmail.com>
2022-01-18 22:53:05 -0800

    made changes to server.js

---
+++ routes/invoices.js
@@ -0,0 +1,16 @@
+// const jsonServer = require("json-server");
+// const router = jsonServer.router("db.json");
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const users = await fetch("http://localhost:3001/invoices");
+//     const data = await users.json();
+
+//     return data;
+//   } catch (err) {
+//     console.error(err.messge);
+//     res.status(500).send("Server Error");
+//   }
+// });
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,54 @@
 require("dotenv").config();
 
-const jsonServer = require("json-server");
-const path = require("path");
+// const jsonServer = require("json-server");
+// const path = require("path");
+// const express = require("express");
+// const app = express();
+// const bodyParser = require("body-parser");
+// const router = jsonServer.router("db.json");
+
+// const PORT = process.env.PORT || 3000;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(express.static(path.join(__dirname, "build")));
+
+// app.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   })
+// );
+
+// app.use("/api", jsonServer.router("db.json"));
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.join(__dirname, "build", "index.html"))
+//   );
+// }
+
+// app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+
 const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const router = jsonServer.router("db.json");
+const jsonServer = require("json-server");
 
-const PORT = process.env.PORT || 3001;
+const server = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT;
+// ...
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
-app.use("/", router);
+// You may want to mount JSON Server on a specific end-point, for example /api
+// Optiona,l except if you want to have JSON Server defaults
+// server.use('/api', jsonServer.defaults());
+server.use("/api", jsonServer.router("db.json"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-  app.get("*", (req, res) =>
+  server.use(express.static(path.join(__dirname, "build")));
+  server.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "build", "index.html"))
   );
 }
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+server.listen(PORT, () => console.log(`Server started on PORT 3000`));
