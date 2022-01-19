38d2a19f80fe1b356ef53db8cce82d7379cb7b24
Adam <adamjwatt88@gmail.com>
2022-01-18 23:19:00 -0800

    made changes to server.js

--- server.js
+++ server.js
@@ -1,56 +1,32 @@
 require("dotenv").config();
 
-// const jsonServer = require("json-server");
-// const path = require("path");
-// const express = require("express");
-// const app = express();
-// const bodyParser = require("body-parser");
-// const router = jsonServer.router("db.json");
-
-// const PORT = process.env.PORT || 3000;
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(express.static(path.join(__dirname, "build")));
-
-// app.use(
-//   jsonServer.rewriter({
-//     "/api/*": "/$1",
-//   })
-// );
-
-// app.use("/api", jsonServer.router("db.json"));
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "build")));
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.join(__dirname, "build", "index.html"))
-//   );
-// }
-
-// app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
-
-const express = require("express");
 const jsonServer = require("json-server");
 const path = require("path");
-const server = express();
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
+const router = jsonServer.router("db.json");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-server.use(bodyParser.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// You may want to mount JSON Server on a specific end-point, for example /api
-// Optiona,l except if you want to have JSON Server defaults
-// server.use('/api', jsonServer.defaults());
-server.use("/api", jsonServer.router("db.json"));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+
+app.use("/api", jsonServer.router("db.json"));
 
 if (process.env.NODE_ENV === "production") {
-  server.use(express.static(path.join(__dirname, "build")));
-  server.get("*", (req, res) =>
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "build", "index.html"))
   );
 }
 
-server.listen(PORT, () => console.log(`Server started on PORT 3000`));
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
